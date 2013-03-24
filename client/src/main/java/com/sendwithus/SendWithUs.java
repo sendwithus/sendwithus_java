8fb14d9236bd45057bc94c3f196a017e362b12bc
Shea Phillips <wombat@Sheas-MacBook-Pro.local>
2013-03-24 11:12:09 -0700

    Finished mavenization.

--- client/src/main/java/com/sendwithus/SendWithUs.java
+++ client/src/main/java/com/sendwithus/SendWithUs.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
