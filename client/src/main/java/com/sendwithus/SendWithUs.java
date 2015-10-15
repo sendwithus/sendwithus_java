a76eeef6112746a26f118c833dc5a14245af384e
Marie Starck <marie.compsci14@gmail.com>
2015-10-15 10:00:56 -0700

    increase version

--- client/src/main/java/com/sendwithus/SendWithUs.java
+++ client/src/main/java/com/sendwithus/SendWithUs.java
@@ -38,7 +38,7 @@ public class SendWithUs
     public static final String API_PORT = "443";
     
     public static final String API_VERSION = "1";
-    public static final String CLIENT_VERSION = "1.8.0";
+    public static final String CLIENT_VERSION = "1.9.0";
     public static final String CLIENT_LANG = "java";
     public static final String SWU_API_HEADER = "X-SWU-API-KEY";
     public static final String SWU_CLIENT_HEADER = "X-SWU-API-CLIENT";
